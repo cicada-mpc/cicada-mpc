72d2f1f8a9a961fe9c3ffbc226c34bc5e1473267
Ken Goss <kgoss@sandia.gov>
2021-07-06 15:16:37 -0600

    added exponentiation function and example script

---
+++ examples/exp.py
@@ -0,0 +1,47 @@
+# Copyright 2021 National Technology & Engineering Solutions
+# of Sandia, LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS,
+# the U.S. Government retains certain rights in this software.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import logging
+
+import numpy
+
+import cicada.additive
+import cicada.communicator
+
+logging.basicConfig(level=logging.INFO)
+
+@cicada.communicator.NNGCommunicator.run(world_size=3)
+def main(communicator):
+    log = cicada.Logger(logging.getLogger(), communicator)
+    protocol = cicada.additive.AdditiveProtocol(communicator)
+    generator = numpy.random.default_rng()
+
+    testvec = numpy.array([-1,2,3,4], dtype=object).reshape(2,2)    
+    powval = 4
+
+    secret_share = protocol.share(src=0, secret=protocol.encoder.encode(testvec), shape=(2,2))
+    secret = protocol.encoder.decode(protocol.reveal(secret_share))
+    log.info(f"Player {communicator.rank} secret: \n{secret}")
+
+    exp_share = protocol.exp(secret_share,powval) 
+
+    exp = protocol.encoder.decode(protocol.reveal(exp_share))
+    log.info(f"Player {communicator.rank} exp: \n{exp}")
+
+    log.info(f"Player {communicator.rank} expected: \n{testvec**powval}")
+main()
+
