46e23b95a6794365db5190018514f700f56f70c9
Timothy M. Shead <tshead@sandia.gov>
2021-11-19 14:27:36 -0700

    Cleanup configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Configuration file for the Sphinx documentation builder.
-#
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
