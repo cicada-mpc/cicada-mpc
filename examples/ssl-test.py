5464f21072a0fe7967a4b1de4a2036e7402fb456
Timothy M. Shead <tshead@sandia.gov>
2022-02-23 12:23:48 -0700

    `cicada start` command can specify TLS certificates.

--- examples/ssl-test.py
+++ examples/ssl-test.py
@@ -29,24 +29,26 @@ world_size = int(os.environ.get("CICADA_WORLD_SIZE"))
 rank = int(os.environ.get("CICADA_RANK"))
 address = os.environ.get("CICADA_ADDRESS")
 root_address = os.environ.get("CICADA_ROOT_ADDRESS")
+identity = os.environ.get("CICADA_IDENTITY")
+peers = os.environ.get("CICADA_PEERS")
 
 server = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-server.load_cert_chain(certfile=f"player-{rank}.pem")
-for index in range(world_size):
-    if index != rank:
-        server.load_verify_locations(f"player-{index}.cert")
+server.load_cert_chain(certfile=identity)
+for peer in peers.split(","):
+    if peer:
+        server.load_verify_locations(peer)
 server.check_hostname=False
 server.verify_mode = ssl.CERT_REQUIRED
 
 client = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-client.load_cert_chain(certfile=f"player-{rank}.pem")
-for index in range(world_size):
-    if index != rank:
-        client.load_verify_locations(f"player-{index}.cert")
+client.load_cert_chain(certfile=identity)
+for peer in peers.split(","):
+    if peer:
+        client.load_verify_locations(peer)
 client.check_hostname = False
 client.verify_mode = ssl.CERT_REQUIRED
 
-timer = connect.Timer(threshold=5)
+timer = connect.Timer(threshold=300)
 listen_socket = connect.listen(address=address, rank=rank, timer=timer)
 sockets = connect.rendezvous(listen_socket=listen_socket, root_address=root_address, world_size=world_size, rank=rank, timer=timer, tls=(server, client))
 with SocketCommunicator(sockets=sockets) as communicator:
