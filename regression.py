508367efa1daa0f1018e033cb7a4311876b717fe
Timothy M. Shead <tshead@sandia.gov>
2022-03-12 23:10:46 -0700

    regression.py allows extra behave arguments.

--- regression.py
+++ regression.py
@@ -18,13 +18,14 @@ import glob
 import os
 import shutil
 import subprocess
+import sys
 
 for path in glob.glob(".coverage*"):
     if path not in [".coveragerc"]:
         os.remove(path)
 if os.path.exists(".cover"):
     shutil.rmtree(".cover")
-subprocess.call(["coverage", "run", "-m", "behave"])
+subprocess.call(["coverage", "run", "-m", "behave"] + sys.argv[1:])
 subprocess.call(["coverage", "combine"])
 subprocess.call(["coverage", "report"])
 subprocess.call(["coverage", "html", "--directory", ".cover"])
