c2f601323754d3121280f10085baf047c3fe977a
Ken Goss <kgoss@sandia.gov>
2023-08-03 14:47:21 -0600

    add notebook drafts and do some bug fixing

--- cicada/additive.py
+++ cicada/additive.py
@@ -1156,7 +1156,7 @@ class AdditiveProtocolSuite(object):
         self._assert_unary_compatible(operand, "operand")
         return self.field_subtract(self.field.full_like(operand.storage, self.field.order), operand)
 
-    def pade_approx(self, func, endpoints, resolution, operand,*, encoding=None, degree=9):
+    def pade_approx(self, func, operand,*, center=0, encoding=None, degree=9):
         """Return the pade approximation of func evaluated at operand.
 
         This is a collective operation that *must* be called
@@ -1179,32 +1179,34 @@ class AdditiveProtocolSuite(object):
             Secret shared result of evaluating the pade approximant of func(operand) with the given parameters
         """
         from scipy.interpolate import approximate_taylor_polynomial, pade
-        axis = numy.linspace(endpoints[0], endpoints[1], resolution)
         num_deg = degree%2+degree//2
         den_deg = degree//2
 
         self._assert_unary_compatible(operand, "operand")
         encoding = self._require_encoding(encoding)
 
-        func_taylor = approximate_taylor_polynomial(func, (endpoints[0]+endpoints[1])/2, degree, degree+1)
+        func_taylor = approximate_taylor_polynomial(func, center, degree, degree+1)
         func_pade_num, func_pade_den = pade(func_taylor, den_deg, n=num_deg)
-        enc_func_pade_num = encoding.encode(numpy.array([x for x in func_pade_num], self.field)
-        enc_func_pade_den = encoding.encode(numpy.array([x for x in func_pade_den], self.field)
-        op_pows_num = [operand]
+        enc_func_pade_num = encoding.encode(numpy.array([x for x in func_pade_num]), self.field)
+        enc_func_pade_den = encoding.encode(numpy.array([x for x in func_pade_den]), self.field)
+        op_pows_num = [self.share(src=1, secret=numpy.array(1), shape=())]
         for i in range(num_deg):
             op_pows_num.append(self.multiply(operand, op_pows_num[-1]))
         if degree%2:
             op_pows_den=[thing for thing in op_pows_num[:-1]]
         else:
             op_pows_den=[thing for thing in op_pows_num]
-        
+        op_pows_num = AdditiveArrayShare(numpy.array([x.storage for x in op_pows_num]))
+        op_pows_den = AdditiveArrayShare(numpy.array([x.storage for x in op_pows_den]))
         result_num_prod = self.field_multiply(op_pows_num, enc_func_pade_num)
         result_num = self.sum(result_num_prod)
 
         result_den_prod = self.field_multiply(op_pows_den, enc_func_pade_den)
         result_den = self.sum(result_den_prod)
-        
-        result = self.divide(result_num, result_den)
+        # the legit thing to do
+        #result = self.divide(result_num, result_den)
+        # the thing I have to do right now for a bit till division is sorted
+        result = self.reveal(result_num)/self.reveal(result_den)
         return result
 
     def power(self, lhs, rhs, *, encoding=None):
@@ -1685,6 +1687,46 @@ class AdditiveProtocolSuite(object):
         """
         self._assert_unary_compatible(operand, "operand")
         return AdditiveArrayShare(self.field.sum(operand.storage))
+
+
+    def taylor_approx(self, func, operand,*, center=0, degree=7, encoding=None):
+
+        """Return the evaluation of a public function evaluated at a shared secret, by taylor approximation
+
+
+        Note
+        ----
+        This is a collective operation that *must* be called
+        by all players that are members of :attr:`communicator`.
+
+        Parameters
+        ----------
+        operand: :class:`AdditiveArrayShare`, required
+            Secret shared array to be summed.
+
+        Returns
+        -------
+        value: :class:`AdditiveArrayShare`
+            Secret-shared sum of `operand`'s elements.
+        """
+        from scipy.interpolate import approximate_taylor_polynomial
+
+        self._assert_unary_compatible(operand, "operand")
+        encoding = self._require_encoding(encoding)
+
+        taylor_poly = approximate_taylor_polynomial(func, center, degree, 1)
+
+        enc_taylor_coef = encoding.encode(numpy.array([x for x in taylor_poly]), self.field)
+        op_pow_list = [self.share(src=1, secret=numpy.array(1), shape=())]
+        for i in range(degree):
+            op_pow_list.append(self.multiply(operand, op_pow_list[-1]))
+
+        op_pow_shares = AdditiveArrayShare(numpy.array([x.storage for x in op_pow_list]))
+            
+        result = self.field_multiply(op_pow_shares, enc_taylor_coef)
+        result = self.sum(result)
+        result = self.right_shift(result, bits=encoding.precision)
+        return result
 
 
 #    def untruncated_divide(self, lhs, rhs, *, rmask=None, mask1=None, rem1=None, mask2=None, rem2=None):
