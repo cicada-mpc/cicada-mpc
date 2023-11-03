88aa39bf9aee1d2292c3829528b63e9056afc808
Timothy M. Shead <tshead@sandia.gov>
2023-11-03 09:52:13 -0600

    Reduce the number and complexity of transcript categories.

--- cicada/transcript.py
+++ cicada/transcript.py
@@ -17,8 +17,16 @@
 """Functionality for generating transcripts of library activity.
 """
 
+import collections
 import enum
 import logging
+
+import hunter
+import numpy
+
+from .additive import AdditiveArrayShare
+from .communicator.interface import tagname
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -26,42 +34,76 @@ logger.propagate = False
 
 
 class Category(enum.Enum):
-    APP = 1
-    PROTOCOL = 2
-    COMMUNICATOR = 3
-    ARITHMETIC = 4
+    GENERIC = 1
+    OPERATION = 2
+    MESSAGE = 3
+
+
+class Placeholder(object):
+    def __bool__(self):
+        return False
+
+    def __format__(self, format_spec):
+        return ""
+
+    def __getattr__(self, key):
+        return Placeholder()
+
+    def __getitem__(self, key):
+        return Placeholder()
+
+
+class Fields(object):
+    def __init__(self, fields):
+        self._fields = dict(fields)
+
+    def _mapvalue(self, value):
+        if isinstance(value, AdditiveArrayShare):
+            return value.storage.tolist()
+        if isinstance(value, numpy.ndarray):
+            return value.tolist()
+        return value
+
+
+    def __getitem__(self, key):
+        if key not in self._fields:
+            return Placeholder()
+        value = self._fields[key]
+        if isinstance(value, list):
+            value = [self._mapvalue(item) for item in value]
+        else:
+            value = self._mapvalue(value)
+        return value
 
 
 class Formatter(object):
-    def __init__(self, appfmt=None, protofmt=None, commfmt=None, mathfmt=None):
-        if appfmt is None:
-            appfmt = "{processName} {msg}"
-        if protofmt is None:
-            protofmt = "{processName} {operation} {operands} {result}"
-        if commfmt is None:
-            commfmt = "{processName} {arrow} {other} {tag} {payload}"
-        if mathfmt is None:
-            mathfmt = "{processName} {operation} {operands} {result}"
+    def __init__(self, fmt=None, opfmt=None, msgfmt=None):
+        if fmt is None:
+            fmt = "Comm {comm.name} player {comm.rank}: {msg}"
+        if opfmt is None:
+            opfmt = "Comm {comm.name} player {comm.rank}: {operation} {operands} {result}"
+        if msgfmt is None:
+            msgfmt = "Comm {comm.name} player {comm.rank}: {arrow} {other} {tag} {payload}"
 
-        self._appfmt = appfmt
-        self._protofmt = protofmt
-        self._commfmt = commfmt
-        self._mathfmt = mathfmt
+        self._fmt = fmt
+        self._opfmt = opfmt
+        self._msgfmt = msgfmt
 
 
     def format(self, record):
-        if record.category == Category.APP:
-            return self._appfmt.format(**record.__dict__)
-        if record.category == Category.PROTOCOL:
-            return self._protofmt.format(**record.__dict__)
-        if record.category == Category.COMMUNICATOR:
-            return self._commfmt.format(**record.__dict__)
-        if record.category == Category.ARITHMETIC:
-            return self._mathfmt.format(**record.__dict__)
+        fields = Fields(record.__dict__)
+
+        if record.category == Category.GENERIC:
+            return self._fmt.format_map(fields)
+        if record.category == Category.OPERATION:
+            return self._opfmt.format_map(fields)
+        if record.category == Category.MESSAGE:
+            return self._msgfmt.format_map(fields)
+
         raise ValueError(f"Unrecognized category: {record.category}")
 
 
-def log(category, message=None, **extra):
+def log(message=None, *, category=Category.GENERIC, **extra):
     if not isinstance(category, Category):
         raise ValueError(f"Expected an instance of cicada.transcript.Category, got {type(category)} instead.")
 
@@ -69,50 +111,15 @@ def log(category, message=None, **extra):
     logger.info(message, extra=extra)
 
 
-#########################################################################################
-# Functionality for non-intrusive transcript-logging using the Hunter library
-
-import hunter
-import numpy
-
-from .communicator.interface import tagname
-
-
-def _argument(value):
-    if isinstance(value, numpy.ndarray):
-        return value.tolist()
-    return value
-
-
 def _arguments(locals, kwargs):
-    return [_argument(locals.get(arg)) for arg in kwargs]
+    return [locals.get(arg) for arg in kwargs]
 
 
-class TraceCall(object):
-    def __init__(self, category, kwargs=None):
-        if kwargs is None:
-            kwargs = []
-        self._category = category
-        self._kwargs = kwargs
-
-    def __call__(self, event):
-        if event.kind != "call":
-            return
-        log(
-            self._category,
-            "",
-            operands = _arguments(event.locals, self._kwargs),
-            operation = event.function_object.__qualname__,
-            result = None,
-            )
-
-
-class TraceCallReturn(object):
-    def __init__(self, category, kwargs=None, result=None):
+class LogResult(object):
+    def __init__(self, kwargs=None, result=None):
         if kwargs is None:
             kwargs = []
 
-        self._category = category
         self._kwargs = kwargs
         self._result = result
         self._stack = []
@@ -126,20 +133,24 @@ class TraceCallReturn(object):
             if self._result is None:
                 result = event.arg
             elif isinstance(self._result, str):
-                result = _argument(event.locals.get(self._result))
+                result = event.locals.get(self._result)
             else:
                 result = _arguments(event.locals, self._result)
 
-            log(
-                self._category,
-                "",
+            kwargs = dict(
+                category = Category.OPERATION,
                 operands = self._stack.pop(),
                 operation = event.function_object.__qualname__,
                 result = result,
-                )
+            )
+
+            if "self" in event.locals and hasattr(event.locals["self"], "communicator"):
+                kwargs["comm"] = event.locals["self"].communicator
+
+            log("", **kwargs)
 
 
-class TraceSendMessage(object):
+class LogSendMessage(object):
     def __call__(self, event):
         if event.kind != "call":
             return
@@ -150,22 +161,21 @@ class TraceSendMessage(object):
         tag = event.locals["tag"]
 
         log(
-            Category.COMMUNICATOR,
             "Sent message",
+            category = Category.MESSAGE,
             arrow = "-->",
-            comm = communicator.name,
+            comm = communicator,
             dir = ">",
             dst = dst,
             other = dst,
             payload = payload,
-            rank = communicator.rank,
             src = communicator.rank,
             tag = tagname(tag),
             verb = "send",
             )
 
 
-class TraceQueueMessage(object):
+class LogReceiveMessage(object):
     def __call__(self, event):
         if event.kind != "call":
             return
@@ -176,45 +186,39 @@ class TraceQueueMessage(object):
         tag = event.locals["tag"]
 
         log(
-            Category.COMMUNICATOR,
             "Received message",
+            category = Category.MESSAGE,
             arrow = "<--",
-            comm = communicator.name,
+            comm = communicator,
             dir = "<",
             dst = communicator.rank,
             other = src,
             payload = payload,
-            rank = communicator.rank,
             src = src,
             tag = tagname(tag),
             verb = "receive",
             )
 
 
-class Trace(hunter.actions.ColorStreamAction):
+class LogEvents(hunter.actions.ColorStreamAction):
     def __init__(self):
         self.mappings = {
-            "AdditiveProtocolSuite.reveal" : TraceCall(Category.PROTOCOL),
-            "AdditiveProtocolSuite.share" : TraceCall(Category.PROTOCOL, kwargs=["src", "secret"]),
-            "Field.add" : TraceCallReturn(Category.ARITHMETIC, kwargs=["lhs", "rhs"]),
-            "Field.inplace_add" : TraceCallReturn(Category.ARITHMETIC, kwargs=["lhs", "rhs"], result="lhs"),
-            "FixedPoint.decode" : TraceCallReturn(Category.ARITHMETIC, kwargs=["array"]),
-            "FixedPoint.encode" : TraceCallReturn(Category.ARITHMETIC, kwargs=["array"]),
-            "PRZSProtocol.__init__" : TraceCall(Category.PROTOCOL),
-            "SocketCommunicator._queue_message" : TraceQueueMessage(),
-            "SocketCommunicator._send" : TraceSendMessage(),
+            "AdditiveProtocolSuite.reveal" : LogResult(kwargs=["share"]),
+            "AdditiveProtocolSuite.share" : LogResult(kwargs=["secret"]),
+            "Field.add" : LogResult(kwargs=["lhs", "rhs"]),
+            "Field.inplace_add" : LogResult(kwargs=["lhs", "rhs"], result="lhs"),
+            "FixedPoint.decode" : LogResult(kwargs=["array"]),
+            "FixedPoint.encode" : LogResult(kwargs=["array"]),
+            "SocketCommunicator._queue_message" : LogReceiveMessage(),
+            "SocketCommunicator._send" : LogSendMessage(),
             }
 
     def __call__(self, event):
-        if not hasattr(event.function_object, "__qualname__"):
-            return
-
-        qualname = event.function_object.__qualname__
-        if qualname not in self.mappings:
-            return
-
-        self.mappings[qualname](event)
+        if hasattr(event.function_object, "__qualname__"):
+            qualname = event.function_object.__qualname__
+            if qualname in self.mappings:
+                self.mappings[qualname](event)
 
 
 def record():
-    return hunter.trace(module_startswith="cicada", kind_in=("call", "return"), action=Trace())
+    return hunter.trace(module_startswith="cicada", kind_in=("call", "return"), action=LogEvents())
